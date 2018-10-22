22dac6b22aa31e2534b3f66134bc43059879821d
SagetheCapricorn <oswitoryio12>
2018-10-22 14:42:46 -0400

    Alittle is done

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,21 @@
 public class Person {
+    private String firstName, familyName;
+    public Person(String firstName, String familyNamw)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getFamilyName()
+    {
+        return familyName;
+    }
+    public void setFamn
 }
