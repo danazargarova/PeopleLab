e7db141e0f6f075b3c27593b9d910f5c3307f710
BT_1N3_12 <jtan5662@bths.edu>
2018-10-23 14:44:26 -0400

    commit and plosh

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,5 +1,24 @@
-public class Classroom extends Person  {
-    private Student students;
-    private  Teacher teacher;
+public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+
+    public Classroom(Student[] stus, Teacher teach) {
+        this.students = stus;
+        this.teacher = teach;
+    }
+
+    public String getSubject() {
+        String sub ="";
+        sub = teacher.getSubject();
+        return sub;
+    }
+
+    public double getAvg() {
+        double classAvg=0;
+        for (int i=0; i < students.length; i++) {
+            classAvg += students[i].getGPA();
+        }
+        return classAvg/students.length;
+    }
 
 }
