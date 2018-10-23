d0f4aea3da801d0ccd33eb5b81e81a80bebb28bd
danazargarova <dzargarova6105@bths.edu>
2018-10-23 14:36:45 -0400

    klefhw

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,6 @@
 public class Person {
     private String firstName, familyName;
-    public Person(String firstName, String familyNamw)
+    public Person(String firstName, String familyName)
     {
         this.firstName = firstName;
         this.familyName = familyName;
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,11 @@
 public class Teacher extends Person{
+    private String Subject;
+    private String Title;
+    String [] Title={"Mr", "Ms", "Mx", "Mrs"};
+
+    public Teacher(String firstName, String familyName, String subject, String title) {
+        super(firstName, familyName);
+        Subject = subject;
+        Title = title;
+    }
 }
