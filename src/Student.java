13a0746aff42bb22d5c203c7acb5afb81c371711
SagetheCapricorn <oswitoryio12>
2018-10-23 14:43:46 -0400

    12151215

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,21 @@
 public class Student extends Person {
+    private double GPA;
+    private int age;
+    private int grade;
 
+    public Student(String firstName, String familyName, double GPA, int age, int grade) {
+        super(firstName, familyName);
+        this.GPA = GPA;
+        this.age = age;
+        this.grade = grade;
+    }
 
+    public double getGPA() {
+        return GPA;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
