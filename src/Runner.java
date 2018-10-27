3c3ffdd6ddc6cd8928e7a666892ad4f7406f5f5e
danazargarova <dzargarova6105@bths.edu>
2018-10-27 19:49:40 -0400

    yoooo

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,6 @@ public class Runner {
         Student student1= new Student("Sage", "Basil", 2.0, 17, 12);
         Student student2= new Student("Dana", "White", 4.0, 17, 12);
         Teacher teacher= new Teacher("Nathan", "Levin", "APCS", "Mr.");
-        Classroom classroom = new Classroom("Spray", "");
+        Classroom classroom = new Classroom("Spray Tan", " ", "Mr. Levin");
     }
 }
