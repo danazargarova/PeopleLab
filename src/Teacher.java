ad4949158aa57498485b7008b1f4f162fe85b3db
danazargarova <dzargarova6105@bths.edu>
2018-10-27 19:05:04 -0400

    yoooo

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,7 +1,6 @@
 public class Teacher extends Person{
     private String Subject;
     private String Title;
-    String [] Title={"Mr", "Ms", "Mx", "Mrs"};
 
     public Teacher(String firstName, String familyName, String subject, String title) {
         super(firstName, familyName);
