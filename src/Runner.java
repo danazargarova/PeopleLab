d021ffd1e33695e7c4fc7231f670c1f610033958
Jason <jtan5662@bths.edu>
2018-10-27 19:41:03 -0400

    AAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-        Student student = new Student ("Jason", "Tan");
-        Student student1= new Student("Sage", "Basil");
-        Student student2= new Student("Dana", "White");
+        Student student = new Student ("Spray", "Tan", 3.0, 17, 12);
+        Student student1= new Student("Sage", "Basil", 2.0, 17, 12);
+        Student student2= new Student("Dana", "White", 1.0, 17, 12);
         Teacher teacher= new Teacher("Nathan", "Levin", "APCS", "Mr.");
         Classroom classroom = new Classroom;
     }
