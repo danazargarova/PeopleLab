4d3aaf9e5782b768835213be7b10672feb3c457a
danazargarova <dzargarova6105@bths.edu>
2018-10-27 19:47:53 -0400

    yoooo

--- src/Runner.java
+++ src/Runner.java
@@ -2,8 +2,8 @@ public class Runner {
     public static void main(String[] args) {
         Student student = new Student ("Spray", "Tan", 3.0, 17, 12);
         Student student1= new Student("Sage", "Basil", 2.0, 17, 12);
-        Student student2= new Student("Dana", "White", 1.0, 17, 12);
+        Student student2= new Student("Dana", "White", 4.0, 17, 12);
         Teacher teacher= new Teacher("Nathan", "Levin", "APCS", "Mr.");
-        Classroom classroom = new Classroom("S", "");
+        Classroom classroom = new Classroom("Spray", "");
     }
 }
