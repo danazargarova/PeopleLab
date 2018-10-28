e575d65158aaf01f4703d30675aed660d4701e75
Sage <scharitygriffit3519@bths.edu>
2018-10-28 19:29:32 -0400

    xvsdgsd

--- src/Runner.java
+++ src/Runner.java
@@ -1,24 +1,25 @@
 public class Runner {
-    public static String[] firstNames = {"Jason","Dana", "Sage","Tobey", "Sam", "Holly"};
-    public static String[] lastNames = {"Tan", "White","Capricorn", "Mcguire", "Reimi","Shoot"};
+    public static String[] firstNames = {"Jason", "Dana", "Sage", "Tobey", "Sam", "Holly"};
+    public static String[] lastNames = {"Tan", "White", "Capricorn", "Mcguire", "Reimi", "Shoot"};
 
-    public static String[] classes= {"Math", "English", "Science", "Economics","APCS"};
+    public static String[] classes = {"Math", "English", "Science", "Economics", "APCS"};
 
     public static Student ranStudent() {
-    Student newStudent= new Student(randomfirstNames(), randomlastNames());
-    return newStudent;
+        Student newStudent = new Student(randomfirstNames(), randomlastNames());
+        return newStudent;
     }
 
     public static String randomfirstNames() {
-        return firstNames((int)(Math.random()*firstNames.length));
+        return firstNames((int) (Math.random() * firstNames.length));
     }
-}
+
 
     public static void main(String[] args) {
-            Teacher classTeacher = new Teacher(firstNames(),lastNames(),"Computer Science","Mr");
-            Student[] students = new Student[6];
-            for(int i = 0; i < students.length; i++){
-                students[i] = randomStudent();
+        Teacher classTeacher = new Teacher(firstNames(), lastNames(), "Computer Science", "Mr");
+        Student[] students = new Student[6];
+        for (int i = 0; i < students.length; i++) {
+            students[i] = randomStudent();
 
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,14 @@ public class Student extends Person {
     private int age;
     private int grade;
 
+    /**
+     * This method creates a student that has a first and last name,GPA,Grade Level, age
+     * @param firstName this is the students fitst name .
+     * @param familyName this is the students last name.
+     * @param GPA the students grade point average.
+     * @param age this is the students age.
+     * @param grade the grade the student is in.
+     */
     public Student(String firstName, String familyName, double GPA, int age, int grade) {
         super(firstName, familyName);
         this.GPA = GPA;
